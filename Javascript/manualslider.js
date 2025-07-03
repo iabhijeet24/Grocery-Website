8fa70ea99692c091972d738d30611ef80ddc4b76
Abhijeet Singh <inabhijeet241107@gmail.com>
2025-07-03 10:55:11 +0530

    Updated CSS

--- Javascript/manualslider.js
+++ Javascript/manualslider.js
@@ -2,7 +2,7 @@
     const track = document.getElementById("manualsliderTrack");
     const images = [...track.children];
 
-    // Clone all images and append them for seamless looping
+    
     images.forEach((img) => {
       const clone = img.cloneNode(true);
       track.appendChild(clone);
